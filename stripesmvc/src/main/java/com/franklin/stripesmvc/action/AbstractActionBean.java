a69a59d2f97cfa09fc60ab04eae0368662994850
Jesse Seger <jesses@hawkridgesys.com>
2013-12-11 15:48:36 -0500

    #9 cleaned up file

--- stripesmvc/src/main/java/com/franklin/stripesmvc/action/AbstractActionBean.java
+++ stripesmvc/src/main/java/com/franklin/stripesmvc/action/AbstractActionBean.java
@@ -6,13 +6,8 @@ package com.franklin.stripesmvc.action;
 
 
 import com.franklin.stripesmvc.ext.ActionBeanContextImpl;
-import com.franklin.stripesmvc.ext.MyLocalePicker;
-import java.util.HashMap;
-import java.util.Map;
-import javax.servlet.http.HttpServletRequest;
 import net.sourceforge.stripes.action.ActionBean;
 import net.sourceforge.stripes.action.ActionBeanContext;
-import net.sourceforge.stripes.integration.spring.SpringBean;
 
 /**
  *
@@ -33,5 +28,5 @@ public abstract class AbstractActionBean implements ActionBean {
     public ActionBeanContextImpl getContext() {
         return context;
     }
-
+    
 }
